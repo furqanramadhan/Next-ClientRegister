824b38381f6e678f7bc505f21f426fc54a367d85
Furqan Ramadhan <furqan21@mhs.usk.ac.id>
2024-08-12 00:49:44 +0700

    add delete details

---
+++ src/app/details/DropDetails.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+type Detail = {
+  id: number;
+  title: string;
+  price: number;
+};
+
+export default function DropDetails(detail: Detail) {
+  const [modal, setModal] = useState(false);
+  const [isMutating, setIsMutating] = useState(false);
+
+  const router = useRouter();
+
+  async function handleDelete(detailId: number) {
+    setIsMutating(true);
+
+    await fetch(`http://localhost:5000/details/${detailId}`, {
+      method: "DELETE",
+    });
+
+    setIsMutating(false);
+
+    router.refresh();
+    setModal(false);
+  }
+
+  function handleChange() {
+    setModal(!modal);
+  }
+
+  return (
+    <div>
+      <button className="btn btn-error btn-sm" onClick={handleChange}>
+        Delete
+      </button>
+
+      <input
+        type="checkbox"
+        checked={modal}
+        onChange={handleChange}
+        className="modal-toggle"
+      />
+
+      <div className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">
+            Are sure to delete {detail.title} ?
+          </h3>
+          <div className="modal-action">
+            <button type="button" className="btn" onClick={handleChange}>
+              Close
+            </button>
+            {!isMutating ? (
+              <button
+                type="button"
+                onClick={() => handleDelete(detail.id)}
+                className="btn btn-primary"
+              >
+                Delete
+              </button>
+            ) : (
+              <button type="button" className="btn loading">
+                Deleting...
+              </button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/app/details/page.tsx
+++ src/app/details/page.tsx
@@ -1,4 +1,5 @@
 import AddDetails from "./AddDetails";
+import DropDetails from "./DropDetails";
 
 type Detail = {
   id: number;
@@ -7,20 +8,31 @@ type Detail = {
 };
 
 async function getDetails() {
-  const res = await fetch("http://localhost:5000/details", {
-    cache: "no-store",
-  });
-  const data = await res.json();
-  console.log(data);
-  return data;
+  try {
+    const res = await fetch(`http://localhost:5000/details`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch details");
+    }
+
+    const data = await res.json();
+    console.log(data); // Debugging data structure
+    return data;
+  } catch (error) {
+    console.error("Error fetching details:", error);
+    return []; // Return an empty array in case of error
+  }
 }
 
 export default async function ProductList() {
   const details: Detail[] = await getDetails();
+
   return (
     <div className="px-10 py-10">
       <div className="pd-2">
-        <AddDetails></AddDetails>
+        <AddDetails />
       </div>
       <div className="overflow-hidden rounded-lg shadow-lg">
         <table className="table table-fixed w-full">
@@ -33,14 +45,17 @@ export default async function ProductList() {
             </tr>
           </thead>
           <tbody>
-            {details.map((detail, index) => (
+            {details.map((detail) => (
               <tr
                 key={detail.id}
                 className="bg-white border-b last:border-none hover:bg-gray-50"
               >
-                <td className="px-4 py-2 text-left">{index + 1}</td>
+                <td className="px-4 py-2 text-left">{detail.id}</td>
                 <td className="px-4 py-2 text-left">{detail.title}</td>
                 <td className="px-4 py-2 text-left">{detail.price}</td>
+                <td>
+                  <DropDetails {...detail} />
+                </td>
               </tr>
             ))}
           </tbody>
